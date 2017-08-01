57144f025a2d1a7ffc415753473da17724c44837
Dirkster99 <Dirk.Bahle@gmx.de>
2017-08-01 16:15:48 +0200

    Version adjusted

--- source/DropDownButtonLib/Properties/AssemblyInfo.cs
+++ source/DropDownButtonLib/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DropDownButtonLib")]
-[assembly: AssemblyCopyright("Copyright © Open Source 2014")]
+[assembly: AssemblyCopyright("Copyright © Open Source 2014-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,8 +33,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located
